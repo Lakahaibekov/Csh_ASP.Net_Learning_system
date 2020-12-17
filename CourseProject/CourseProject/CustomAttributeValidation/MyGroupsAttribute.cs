f55b94c51ba0d7e3ad6a24cd1cdeffcb2ab91fac
Lakahaibekov <67503549+Lakahaibekov@users.noreply.github.com>
2020-12-17 11:58:21 +0600

    Final version
    Final version

---
+++ CourseProject/CourseProject/CustomAttributeValidation/MyGroupsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseProject.CustomAttributeValidation
+{
+    public class MyGroupsAttribute : ValidationAttribute
+    {
+        private static string[] myGroups;
+
+        public MyGroupsAttribute(string[] Groups)
+        {
+            myGroups = Groups;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                string strval = value.ToString();
+                for (int i = 0; i < myGroups.Length; i++)
+                {
+                    if (strval == myGroups[i])
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CourseProject/CourseProject/Migrations/202012111120093_MigrateDB.cs
@@ -0,0 +1,138 @@
+﻿namespace CourseProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrateDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Lessons",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseId = c.Int(),
+                        TeacherId = c.Int(),
+                        GroupId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Courses", t => t.CourseId)
+                .ForeignKey("dbo.Teachers", t => t.TeacherId)
+                .ForeignKey("dbo.Groups", t => t.GroupId)
+                .Index(t => t.CourseId)
+                .Index(t => t.TeacherId)
+                .Index(t => t.GroupId);
+            
+            CreateTable(
+                "dbo.Groups",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        AdviserId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Teachers", t => t.AdviserId)
+                .Index(t => t.AdviserId);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                        ParentId = c.Int(),
+                        GroupId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Groups", t => t.GroupId)
+                .ForeignKey("dbo.Parents", t => t.ParentId)
+                .Index(t => t.ParentId)
+                .Index(t => t.GroupId);
+            
+            CreateTable(
+                "dbo.Parents",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Grades",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Value = c.Int(nullable: false),
+                        StudentId = c.Int(),
+                        LessonId = c.Int(),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Lessons", t => t.LessonId)
+                .ForeignKey("dbo.Students", t => t.StudentId)
+                .Index(t => t.StudentId)
+                .Index(t => t.LessonId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Grades", "StudentId", "dbo.Students");
+            DropForeignKey("dbo.Grades", "LessonId", "dbo.Lessons");
+            DropForeignKey("dbo.Lessons", "GroupId", "dbo.Groups");
+            DropForeignKey("dbo.Students", "ParentId", "dbo.Parents");
+            DropForeignKey("dbo.Students", "GroupId", "dbo.Groups");
+            DropForeignKey("dbo.Groups", "AdviserId", "dbo.Teachers");
+            DropForeignKey("dbo.Lessons", "TeacherId", "dbo.Teachers");
+            DropForeignKey("dbo.Lessons", "CourseId", "dbo.Courses");
+            DropIndex("dbo.Grades", new[] { "LessonId" });
+            DropIndex("dbo.Grades", new[] { "StudentId" });
+            DropIndex("dbo.Students", new[] { "GroupId" });
+            DropIndex("dbo.Students", new[] { "ParentId" });
+            DropIndex("dbo.Groups", new[] { "AdviserId" });
+            DropIndex("dbo.Lessons", new[] { "GroupId" });
+            DropIndex("dbo.Lessons", new[] { "TeacherId" });
+            DropIndex("dbo.Lessons", new[] { "CourseId" });
+            DropTable("dbo.Grades");
+            DropTable("dbo.Parents");
+            DropTable("dbo.Students");
+            DropTable("dbo.Teachers");
+            DropTable("dbo.Groups");
+            DropTable("dbo.Lessons");
+            DropTable("dbo.Courses");
+        }
+    }
+}
